21f0a8c24b76a35714532ded1c8d8fc5330de742
DARTAY-DN <66309262+DARTAY-DN@users.noreply.github.com>
2020-06-03 00:08:36 +0600

    Create towerutils.py

---
+++ towerutils.py
@@ -0,0 +1,82 @@
+import requests
+import datetime
+import sys
+import time
+import argparse
+import os
+import colorama
+import csv
+import json
+import vk
+import random
+from colorama import init
+from colorama import Fore, Back, Style
+init(autoreset=True)
+import itertools
+import threading
+import time
+import base64
+import urllib.request
+from bs4 import BeautifulSoup
+import socket
+
+#желтый
+#\033[33m
+#Зеленый
+#\033[32m = g.c
+#Красный
+#\033[31m = r.c
+#Темно голубой
+#\033[34m = t.g.c
+
+os.system('clear')
+banner = """\033[32m
+▒█▀█▀█▒█▒█▒█▀█▀█░▐██▒██░░░▒▄█▀▀█
+░░▒█░░▒█▒█░░▒█░░─░█▌▒██░░░▒▀▀█▄▄
+░▒▄█▄░▒▀▄▀░▒▄█▄░░▐██▒██▄▄█▒█▄▄█▀
+  		Coded By DARTAY\033[32m"""
+
+print(banner)
+time.sleep(3)
+while True:
+	menu = 			input("""\033[34m_________Menu_________
+\033[34m|\033[31m[\033[33m1\033[31m] - \033[36mKingFish3
+\033[34m_________SmsBomer_________
+\033[34m|\033[31m[\033[33m2\033[31m] - \033[32mSmsHam 
+	\033[34m_________VkPermission_________
+\033[34m|\033[31m[\033[33m3\033[31m] - \033[32mVkTool
+\033[34m_________Hack_________
+\033[34m|\033[31m[\033[33m4\033[31m] - \033[32mSherlock 
+\033[34m|\033[31m[\033[33m5\033[31m] - \033[32mMailSpam """)
+	if menu == ('1'):
+		os.system('clear')
+		print('\033[32mЗапуск KingFish3...')
+		os.chdir('utils')
+		os.chdir('kingfish3')
+		os.chdir('kingfish3.0')
+		time.sleep(2)
+		os.system('python fsh.py')
+	elif menu == ('2'):
+		os.system('clear')
+		print('\033[32mЗапуск SmsHam...')
+		os.chdir('utils')
+		os.chdir('smsham')
+		time.sleep(2)
+		os.system('python smsham.py')
+	elif menu == ('3'):
+		os.system('clear')
+		print('\033[32mЗапуск VkTool   ...')
+		os.chdir('utils')
+		os.chdir('VKTOOL')
+		os.system('python n.py')
+	elif menu == ('4'):
+		os.system('clear')
+		print('\033[32mЗапуск Sherlock...')
+		os.chdir('utils')
+		os.chdir('sherlock')
+		os.system('python sherl.py')
+	elif menu == ('5'):
+		os.system('clear')
+		os.chdir('utils')
+		os.chdir('InfinityMailSpam')
+		os.system('python MailSpam.py')
